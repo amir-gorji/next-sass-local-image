b50e5d1e37c9194f7710cb5f2c22c4c21fd683a9
Amir Gorji <amir1gorji@gmail.com>
2020-09-01 09:49:56 +0430

    configurations set

---
+++ src/pages/mori.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../styles/mori.scss'
+
+const Mori = () => {
+  return (
+    <div className="mori_test">
+      <span className="mori-span">
+        nief
+      </span>
+      <img src={require('../assets/images/barg.jpg')} />
+    </div>
+  );
+}
+
+export default Mori;
